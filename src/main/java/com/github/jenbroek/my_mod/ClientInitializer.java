06af673ba4017ac568b5de1914536d509cd076c8
Jens Broekens <jens.broekens@protonmail.com>
2024-03-12 20:54:16 +0100

    initial commit

---
+++ src/main/java/com/github/jenbroek/my_mod/ClientInitializer.java
@@ -0,0 +1,11 @@
+package com.github.jenbroek.my_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ClientInitializer implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+
+}
