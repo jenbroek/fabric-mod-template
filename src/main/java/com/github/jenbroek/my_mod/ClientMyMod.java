0d3b90efcd9cd1456a5937cdf2de7d2de3d6f1f0
Jens Broekens <jens.broekens@protonmail.com>
2025-01-30 04:39:49 +0100

    use prefixed mod name instead for client entrypoint

--- src/main/java/com/github/jenbroek/my_mod/ClientMyMod.java
+++ src/main/java/com/github/jenbroek/my_mod/ClientMyMod.java
@@ -2,7 +2,7 @@ package com.github.jenbroek.my_mod;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public final class ClientInitializer implements ClientModInitializer {
+public final class ClientMyMod implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
